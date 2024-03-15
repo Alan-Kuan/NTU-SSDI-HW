bedd66060f0b30c0ec535ddc7b115dce6b605e7f
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-15 17:22:12 +0800

    style: change tabs to spaces

--- hw2/rootkit.c
+++ hw2/rootkit.c
@@ -24,64 +24,64 @@ struct cdev *kernel_cdev;
 
 static int rootkit_open(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "%s\n", __func__);
-	return 0;
+    printk(KERN_INFO "%s\n", __func__);
+    return 0;
 }
 
 static int rootkit_release(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "%s\n", __func__);
-	return 0;
+    printk(KERN_INFO "%s\n", __func__);
+    return 0;
 }
 
 static long rootkit_ioctl(struct file *filp, unsigned int ioctl,
-			  unsigned long arg)
+                          unsigned long arg)
 {
-	printk(KERN_INFO "%s\n", __func__);
-	return 0;
+    printk(KERN_INFO "%s\n", __func__);
+    return 0;
 }
 
 struct file_operations fops = {
-	open: rootkit_open,
-	unlocked_ioctl: rootkit_ioctl,
-	release: rootkit_release,
-	owner: THIS_MODULE
+    open: rootkit_open,
+    unlocked_ioctl: rootkit_ioctl,
+    release: rootkit_release,
+    owner: THIS_MODULE
 };
 
 static int __init rootkit_init(void)
 {
-	int ret;
-	dev_t dev_no, dev;
+    int ret;
+    dev_t dev_no, dev;
 
-	kernel_cdev = cdev_alloc();
-	kernel_cdev->ops = &fops;
-	kernel_cdev->owner = THIS_MODULE;
+    kernel_cdev = cdev_alloc();
+    kernel_cdev->ops = &fops;
+    kernel_cdev->owner = THIS_MODULE;
 
-	ret = alloc_chrdev_region(&dev_no, 0, 1, "rootkit");
-	if (ret < 0) {
-		pr_info("major number allocation failed\n");
-		return ret;
-	}
+    ret = alloc_chrdev_region(&dev_no, 0, 1, "rootkit");
+    if (ret < 0) {
+        pr_info("major number allocation failed\n");
+        return ret;
+    }
 
-	major = MAJOR(dev_no);
-	dev = MKDEV(major, 0);
-	printk("The major number for your device is %d\n", major);
-	ret = cdev_add(kernel_cdev, dev, 1);
-	if (ret < 0) {
-		pr_info(KERN_INFO "unable to allocate cdev");
-		return ret;
-	}
+    major = MAJOR(dev_no);
+    dev = MKDEV(major, 0);
+    printk("The major number for your device is %d\n", major);
+    ret = cdev_add(kernel_cdev, dev, 1);
+    if (ret < 0) {
+        pr_info(KERN_INFO "unable to allocate cdev");
+        return ret;
+    }
 
-	return 0;
+    return 0;
 }
 
 static void __exit rootkit_exit(void)
 {
-	// TODO: unhook syscall
+    // TODO: unhook syscall
 
-	pr_info("%s: removed\n", OURMODNAME);
-	cdev_del(kernel_cdev);
-	unregister_chrdev_region(major, 1);
+    pr_info("%s: removed\n", OURMODNAME);
+    cdev_del(kernel_cdev);
+    unregister_chrdev_region(major, 1);
 }
 
 module_init(rootkit_init);
